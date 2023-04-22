96ba65ce8c6451bbe68550f9a21d1ee96e9c85d3
Matheus Torresi <matheus.torresi@C02FH1BTML7J.local>
2023-04-22 14:14:37 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,17 @@
+//tipo de dado
+// booleanos
+
+// conversão implicita
+const numero = 456;
+const numeroString = "456";
+
+//Number()
+//String()
+console.log(numero + Number(numeroString))
+
+// == valor independente do tipo de variável
+// === valor considerando tipo de var
+
+
+
+// conversão explícita
